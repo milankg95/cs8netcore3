68125f73d9401a22f00ef6db2ebb5366b3572858
Milan STOJANOVIC <milanstojanovicdm@gmail.com>
2021-11-17 18:29:22 +0100

    Chapter02 finished.

---
+++ code/Chapter02/NullHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+#nullable enable
+
+namespace NullHandling
+{
+
+    class Address {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            // thisCannotBeNull = null;
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            // address.City = null;
+            // address.Region = null;
+
+            string authorName = null;
+            // int x = authorName.Length;
+
+            // int?y = authorName?.Length;
+
+            var result = authorName?.Length ?? 3;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ code/Chapter02/Variables/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88;
+            object name = "Amir";
+
+            Console.WriteLine($"{name} is {height} metres tall.");
+
+            // int length1 = name.Length;
+            int length2 = ((string)name).Length;
+
+            Console.WriteLine($"{name} has {length2} characters.");
+
+            dynamic anotherName = "Ahmed";
+
+            int length = anotherName.Length;
+
+            int population = 66_000_000;
+            double weight = 1.88;
+            decimal price = 4.99M;
+            string fruit = "Apples";
+            char letter = 'Z';
+            bool happy = true;
+
+            var devName = "Milan";
+
+            Console.WriteLine(population);
+            Console.WriteLine(weight);
+            Console.WriteLine(price);
+            Console.WriteLine(fruit);
+            Console.WriteLine(letter);
+            Console.WriteLine(happy);
+            Console.WriteLine(devName);
+
+            Console.WriteLine($"default (int) = {default(int)}");
+            Console.WriteLine($"default (bool) = {default(bool)}");
+            Console.WriteLine($"default (DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default (string) = {default(string)}");
+
+        }
+    }
+}
